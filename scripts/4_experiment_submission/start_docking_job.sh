775199b8b372007039e06ddfa68b919ce126d602
Colby Ford <colbytylerford@gmail.com>
2023-12-11 13:33:16 -0500

    Create start_docking_job.sh

---
+++ scripts/4_experiment_submission/start_docking_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=cford_pd1_iter_$1
+#SBATCH --partition=Orion
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --constraint=epyc
+#SBATCH --mem=500gb
+#SBATCH --time=8:00:00
+
+export SINGULARITY_CONTAINER_HOME=/home/$USER/PD1_Fab_Diffusion/
+module load singularity
+
+singularity run $SINGULARITY_CONTAINER_HOME/haddock.sif
+
+cd $2
+./run-docking.csh
