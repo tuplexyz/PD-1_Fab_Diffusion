229c3b6f4137656d9e498a2bb8517db1c1042c2b
Colby Ford <colbytylerford@gmail.com>
2023-12-12 09:53:17 -0500

    Update submit_experiments.sh

--- scripts/4_experiment_submission/submit_experiments.sh
+++ scripts/4_experiment_submission/submit_experiments.sh
@@ -6,6 +6,9 @@
 #SBATCH --constraint=skylake
 #SBATCH --time=2:00:00
 
+module load singularity
+
+export PROJECT_DIR=/users/$USER/PD1_Fab_Diffusion
 export input_csv=./experiments.csv
 
 while IFS="," read -r antibody_id experiment_path
@@ -14,6 +17,6 @@ do
   echo "Experiment Path: $experiment_path"
   echo ""
 
-  JOBID=$(sbatch --parsable submit_docking_job.sh $antibody_id $experiment_path)
+  JOBID=$(sbatch --parsable $PROJECT_DIR/scripts/4_experiment_submission/start_docking_job.sh $antibody_id $experiment_path)
 
 done < <(tail -n +2 $input_csv)
